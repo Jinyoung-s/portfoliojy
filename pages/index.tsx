0bee099aa7f3943d9942d51f43af5cb3a218cd53
Jinyoung So <sojinyoung23@gmail.com>
2023-02-27 17:37:52 -0500

    adding title page, installing AOS library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,19 @@
 import Head from "../components/Head";
+import { useEffect } from "react";
+
+// Animation package
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 50,
+    });
+  }, []);
+
   return (
     <div>
       <Head />
